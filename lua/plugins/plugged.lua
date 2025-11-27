35526bc4c6420327ed605a75ec5efa08811c9673
Eduardo-Jimenez-M2010 <laloxzwbl@gmail.com>
2025-11-26 23:41:33 -0600

    refactor!(plugin): the plugin luochen1990/rainbow was removed
    Due to the plugin already wasn't used, was removed from plugged.lua
    plugin importation file and its configuration.

--- lua/plugins/plugged.lua
+++ lua/plugins/plugged.lua
@@ -9,7 +9,6 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
     Plug ('agude/vim-eldar')
 
     Plug ('nvim-tree/nvim-tree.lua')
-    Plug ('luochen1990/rainbow')
     Plug ('vim-airline/vim-airline') -- bottom status line
     Plug ('tmsvg/pear-tree')
     Plug ('alvan/vim-closetag')
@@ -38,41 +37,6 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
     Plug ('rafamadriz/friendly-snippets')
     Plug ('hrsh7th/nvim-cmp')
 vim.call('plug#end')
-
-
--- Raunbow pluging settings
-vim.g.rainbow_active = 1
--- vim.g:rainbow_conf = {
---   guifgs = ['royalblue3', 'darkorange3', 'seagreen3', 'firebrick'],
---   ctermfgs = ['lightblue', 'lightyellow', 'lightcyan', 'lightmagenta'],
---   guis = [''],
---   cterms = [''],
---   operators = '_,_',
---   parentheses = ['start=/(/ end=/)/ fold', 'start=/\[/ end=/\]/ fold', 'start=/{/ end=/}/ fold'],
---   separately = {
---      '*': {},
---      'markdown': {
---          'parentheses_options': 'containedin=markdownCode contained', "enable rainbow for code blocks only
---      },
---      'lisp': {
---          'guifgs': ['royalblue3', 'darkorange3', 'seagreen3', 'firebrick', 'darkorchid3'], "lisp needs more colors for parentheses :)
---\        },
---\        'haskell': {
---\            'parentheses': ['start=/(/ end=/)/ fold', 'start=/\[/ end=/\]/ fold', 'start=/\v\{\ze[^-]/ end=/}/ fold'], "the haskell lang pragmas should be excluded
---\        },
---\        'vim': {
---\            'parentheses_options': 'containedin=vimFuncBody', "enable rainbow inside vim function body
---\        },
---\        'perl': {
---\            'syn_name_prefix': 'perlBlockFoldRainbow', "solve the [perl indent-depending-on-syntax problem](https://github.com/luochen1990/rainbow/issues/20)
---\        },
---\        'stylus': {
---\            'parentheses': ['start=/{/ end=/}/ fold contains=@colorableGroup'], "[vim css color](https://github.com/ap/vim-css-color) compatibility
---\        },
---\        'css': 0, "disable this plugin for css files
---\    }
--- }
-
 -- NvimTree
 local function my_on_attach(bufnr)
   local api = require "nvim-tree.api"
