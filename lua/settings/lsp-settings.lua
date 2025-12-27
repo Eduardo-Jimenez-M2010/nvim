8ab4c8ceda3cb446e1e70235e5a2929aeab6d415
Eduardo-Jimenez-M2010 <laloxzwbl@gmail.com>
2025-12-27 16:12:22 -0600

    feat: update lsp api usage and code simplifying
    Due to lsp-config change its api, I updated the use of lsp-config old
    api to the new one in languages configuration into lsp-settings.lua file.

--- lua/settings/lsp-settings.lua
+++ lua/settings/lsp-settings.lua
@@ -17,13 +17,14 @@ require("mason-lspconfig").setup {
     }
 }
 
-local config = require("lspconfig")
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
+-- Setting keybindings for every language
+-- local config = vim.lsp.config()
+-- lcoal config = require("lspconfig")
+-- local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local opts = { noremap = true, silent = true }
 local on_attach = function(client, bufnr)
     opts.buffer = bufnr
 
-    -- set keybinds
     opts.desc = "Show LSP references"
     vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- Show definition, references
 
@@ -32,6 +33,7 @@ local on_attach = function(client, bufnr)
 
     opts.desc = "Show LSP definitions"
     vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
     opts.desc = "Show LSP implementations"
     vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
@@ -63,7 +65,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)
 end
 
-local capabilities = cmp_nvim_lsp.default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local signs = { Error = "", Warn = "", Hint = "󰠠", Info = "" }
 for type, icon in pairs(signs) do
@@ -72,7 +74,7 @@ for type, icon in pairs(signs) do
 end
 
 -- Lua configuration
-config.lua_ls.setup {
+vim.lsp.config('lua_ls', {
     capabilities = capabilities,
     on_attach    = on_attach,
     settings = {
@@ -90,31 +92,37 @@ config.lua_ls.setup {
             }
         }
     }
-}
+})
 
 -- Html configuration
-config.html.setup {
+vim.lsp.config('html', {
     capabilities = capabilities,
     on_attach    = on_attach,
-}
+})
 
-config.tsserver.setup {
+vim.lsp.config('biome', {
     capabilities = capabilities,
     on_attach    = on_attach,
-}
+    cmd = { "biome", "lsp-proxy" },
+    filetypes = {
+        "astro", "css", "graphql", "javascript", "javascriptreact",
+        "json", "jsonc", "svelte", "typescript", "typescript.tsx",
+        "typescriptreact", "vue"
+    }
+})
 
-config.cssls.setup {
+vim.lsp.config('cssls', {
     capabilities = capabilities,
     on_attach    = on_attach,
-}
+})
 
-config.phpactor.setup {
+vim.lsp.config('phpactor', {
     capabilities = capabilities,
     on_attach    = on_attach,
-}
+})
 
 -- C/C++ configuration
-config.clangd.setup {
+vim.lsp.config('clangd', {
     capabilities = capabilities,
     on_attach    = on_attach,
 
@@ -126,38 +134,39 @@ config.clangd.setup {
     args = {
         --"--query-driver=/usr/bin/gcc,/usr/bin/g++",
         --"--extra-arg=-I./lib"
-    }
-}
+    },
+    filetypes = { 'c', 'cpp' }
+})
 
 -- Java configuration
-config.jdtls.setup {
+vim.lsp.config('jdtls', {
     capabilities = capabilities,
     on_attach    = on_attach,
-}
+})
 
 -- Python configuration
-config.pyright.setup {
+vim.lsp.config('pyright', {
     capabilities = capabilities,
     on_attach    = on_attach,
-}
+})
 
-config.texlab.setup {
+vim.lsp.config('texlab', {
     capabilities = capabilities,
     on_attach    = on_attach,
-}
+})
 
 -- BashScrip configuration
-config.bashls.setup {
+vim.lsp.config('bashls', {
     capabilities = capabilities,
     on_attach    = on_attach,
-}
+})
 
 -- Assembler configuration
-config.asm_lsp.setup {
+vim.lsp.config('asm_lsp', {
     filetypes = { "asm", "s", "masm", "nasm" },
     root_dir = config.util.root_pattern(".git") or vim.loop.cwd,
     settings = {
         capabilities = capabilities,
         on_attach    = on_attach,
     }
-}
+})
